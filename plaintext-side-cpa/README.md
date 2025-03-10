1062e2a52e3926b9330e4a64593f8e30b9abfbb4
Prasanna Paithankar <paithankarprasanna@gmail.com>
2025-03-10 20:16:11 +0530

    Assignment 1 final
    Signed-off-by: Prasanna Paithankar <paithankarprasanna@gmail.com>

--- plaintext-side-cpa/README.md
+++ plaintext-side-cpa/README.md
@@ -1,7 +1,7 @@
 # Assignment 1: Plaintext Side CPA
 
 **Description:** A trace file is given containing power traces collected during the execution of AES-128 encryption on an STM32F3 microcontroller interfaced with the ChipWhisperer-Lite. Each row in the provided CSV file consists of the plaintext input, the corresponding ciphertext output, and 5000 sampled power measurements. The dataset includes a total of 5000 such traces.<br>
-Task is to implement a Correlation Power Analysis (CPA) attack from the plaintext side to recover the first byte of the AES-128 key. In the standard CPA approach, the attack exploits the correlation between the measured power consumption and the Hamming weight or Hamming distance of the output of the inverse S-box operation on the XOR result of a ciphertext byte and a key guess. Consider hamming weight as power model.
+***Task is to implement a Correlation Power Analysis (CPA) attack from the plaintext side to recover the first byte of the AES-128 key.*** In the standard CPA approach, the attack exploits the correlation between the measured power consumption and the Hamming weight or Hamming distance of the output of the inverse S-box operation on the XOR result of a ciphertext byte and a key guess. Consider hamming weight as power model.
 
 ### Compilation & Run
 ```bash
@@ -10,8 +10,20 @@ cargo build --release
 ```
 `<path-to-trace-file>` defaults to `src/trace_data_bootcamp_PT5000.csv` if not provided.
 
+### Pre-compiled Binaries
+(Not uploaded on GitHub)<br>
+Pre-compiled binaries for Linux and Windows are available in the `binaries` directory. If they don't work, please compile the code using the above instructions.
+
 ### Results
-For the file `trace_data_bootcamp_PT5000.csv`, the first byte of the key is `to be added`.
+For the file `trace_data_bootcamp_PT5000.csv`, the first byte of the key is `0x13`, i.e. trave 19.
+
+### Notes
+In each trace only the measurements in the range of `window` *(line 178)* are considered for the attack. The values were hardcoded after observing the power traces.
+
+<!-- center the image -->
+<p align="center">
+  <img src="plot1.png">
+</p>
 
 ***
 #### 2025 | Prasanna Paithankar
--- plaintext-side-cpa/src/main.rs
+++ plaintext-side-cpa/src/main.rs
@@ -10,141 +10,211 @@
  */
 
 use csv::ReaderBuilder;
-use ndarray::{Array1, Array2, Zip};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use plotly::{color::NamedColor, common::Marker, layout::Axis, Layout, Plot, Scatter, layout::Annotation};
+use rayon::prelude::*;
 
-fn compute_correlation(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
-    let x_mean = x.mean().unwrap();
-    let y_mean = y.mean().unwrap();
-
-    let numerator = Zip::from(x)
-        .and(y)
-        .map_collect(|&xi, &yi| (xi - x_mean) * (yi - y_mean))
-        .sum();
-
-    let denominator = (Zip::from(x)
-        .map_collect(|&xi| (xi - x_mean).powi(2))
-        .sum()
-        .sqrt())
-        * (Zip::from(y)
-            .map_collect(|&yi| (yi - y_mean).powi(2))
-            .sum()
-            .sqrt());
-
-    if denominator == 0.0 {
-        0.0
-    } else {
-        numerator / denominator
-    }
-}
-
-const SBOX_INV: [u8; 256] = [
-    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
-    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
-    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
-    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
-    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
-    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
-    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
-    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
-    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
-    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
-    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
-    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
-    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
-    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
-    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
-    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
+// AES S-Box
+const SBOX: [u8; 256] = [
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
 ];
 
 fn hamming_weight(byte: u8) -> u8 {
     byte.count_ones() as u8
 }
 
-fn read_trace_file(filename: &str) -> (Vec<[u8; 16]>, Vec<[u8; 16]>, Array2<f64>) {
+fn read_trace_file(
+    filename: &str,
+    window: (usize, usize),
+) -> (Vec<String>, Vec<String>, Vec<Vec<f64>>) {
     let mut rdr = ReaderBuilder::new()
         .has_headers(true)
         .from_path(filename)
         .unwrap();
-    let mut plaintexts = Vec::new();
-    let mut ciphertexts = Vec::new();
-    let mut traces = Vec::new();
+
+    let mut plaintexts: Vec<String> = Vec::new();
+    let mut ciphertexts: Vec<String> = Vec::new();
+    let mut traces: Vec<Vec<f64>> = Vec::new();
 
     for result in rdr.records() {
         let record = result.unwrap();
-        let plaintext: [u8; 16] = record
-            .iter()
-            .take(16)
-            .map(|x| u8::from_str_radix(x, 16).unwrap())
-            .collect::<Vec<u8>>()
-            .try_into()
-            .unwrap();
-        let ciphertext: [u8; 16] = record
-            .iter()
-            .skip(16)
-            .take(16)
-            .map(|x| u8::from_str_radix(x, 16).unwrap())
-            .collect::<Vec<u8>>()
-            .try_into()
-            .unwrap();
+        plaintexts.push(record[0].to_string());
+        ciphertexts.push(record[1].to_string());
+
         let trace: Vec<f64> = record
             .iter()
-            .skip(32)
-            .map(|x| x.parse::<f64>().unwrap())
+            .skip(2 + window.0)
+            .take(window.1 - window.0) // Extracting the window of interest
+            .map(|x| x.parse().unwrap())
             .collect();
-
-        plaintexts.push(plaintext);
-        ciphertexts.push(ciphertext);
         traces.push(trace);
     }
 
-    let trace_matrix = Array2::from_shape_vec(
-        (traces.len(), traces[0].len()),
-        traces.into_iter().flatten().collect(),
-    )
-    .unwrap();
-    (plaintexts, ciphertexts, trace_matrix)
+    (plaintexts, ciphertexts, traces)
 }
 
-fn compute_cpa(plaintexts: &[[u8; 16]], traces: &Array2<f64>) -> u8 {
-    let num_samples = traces.shape()[1];
+// Parallel implementation of correlation matrix computation
+fn compute_correlation_matrix(traces: &Vec<Vec<f64>>, plaintexts: &Vec<String>) -> Vec<Vec<f64>> {
+    let num_traces = traces.len();
+    let num_samples = traces[0].len();
+
+    (0..256)
+        .into_par_iter()
+        .map(|key_byte| {
+            println!("Processing guess key byte: {}", key_byte);
+            (0..num_samples)
+                .into_par_iter()
+                .map(|poi| {
+                    let hypo_vector: Vec<f64> = (0..num_traces)
+                        .into_par_iter()
+                        .map(|i| {
+                            let pt = u128::from_str_radix(&plaintexts[i], 16).unwrap();
+                            let pt_temp = ((pt >> 120) & 0xFF) as u8;
+                            let btemp = SBOX[(pt_temp ^ key_byte as u8) as usize];
+                            hamming_weight(btemp) as f64
+                        })
+                        .collect();
+
+                    let pt_vector: Vec<f64> = traces.par_iter().map(|x| x[poi]).collect();
+                    pearson_correlation(&hypo_vector, &pt_vector)
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn pearson_correlation(x: &Vec<f64>, y: &Vec<f64>) -> f64 {
+    let n = x.len() as f64;
+    let sum_x: f64 = x.iter().sum();
+    let sum_y: f64 = y.iter().sum();
+    let sum_x_sq: f64 = x.iter().map(|x| x * x).sum();
+    let sum_y_sq: f64 = y.iter().map(|y| y * y).sum();
+    let sum_xy: f64 = x.iter().zip(y.iter()).map(|(x, y)| x * y).sum();
+
+    let numerator = n * sum_xy - sum_x * sum_y;
+    let denominator = (n * sum_x_sq - sum_x * sum_x) * (n * sum_y_sq - sum_y * sum_y);
+    let denominator = denominator.sqrt();
+
+    (numerator / denominator).abs()
+}
+
+fn best_key_guess(correlations: &Vec<Vec<f64>>) -> u8 {
     let mut best_key = 0;
-    let mut best_corr: f64 = 0.0;
+    let mut max_correlation = 0.0;
 
-    for key_guess in 0..=255 {
-        let model: Array1<f64> = plaintexts
-            .iter()
-            .map(|pt| hamming_weight(SBOX_INV[(pt[0] ^ key_guess) as usize]) as f64)
-            .collect();
-
-        let correlations: Vec<f64> = (0..num_samples)
-            .into_par_iter()
-            .map(|t| compute_correlation(&model, &traces.column(t).to_owned()))
-            .collect();
-
-        let max_corr = correlations
-            .iter()
-            .cloned()
-            .fold(f64::NEG_INFINITY, f64::max);
-        if max_corr.abs() > best_corr.abs() {
-            best_corr = max_corr;
-            best_key = key_guess;
+    for (key, correlation) in correlations.iter().enumerate() {
+        let max_correlation_byte = correlation.iter().cloned().fold(0.0, f64::max);
+        if max_correlation_byte > max_correlation {
+            max_correlation = max_correlation_byte;
+            best_key = key as u8;
         }
     }
+
     best_key
+}
+
+fn plot_correlation(correlations: &Vec<Vec<f64>>, key: u8, window: (usize, usize)) {
+    let mut traces = Vec::new();
+    for i in 0..correlations.len() {
+        let trace = Scatter::new(
+            (0..correlations[i].len()).collect::<Vec<usize>>(),
+            correlations[i].clone(),
+        );
+        traces.push(trace);
+    }
+
+    let mut plot = Plot::new();
+    for (i, trace) in traces.into_iter().enumerate() {
+        if i == key as usize {
+            plot.add_trace(trace.marker(Marker::new().color(NamedColor::Red)));
+        } else {
+            plot.add_trace(trace);
+        }
+    }
+
+    let tick_values: Vec<f64> = (0..correlations[0].len()).map(|x| x as f64).collect();
+    let tick_text: Vec<String> = (window.0..window.1).map(|x| x.to_string()).collect();
+
+    plot.set_layout(
+        Layout::new()
+            .title("Trace Correlation")
+            .x_axis(Axis::new()
+                .title("Sample Number")
+                .tick_values(tick_values)
+                .tick_text(tick_text)
+            )
+            .y_axis(Axis::new().title("abs(Correlation)"))
+            .annotations(vec![
+                Annotation::new()
+                    .x(2)
+                    .y(0.7)
+                    .text(format!("Best key guess: 0x{:02X}", key))
+                    .show_arrow(false),
+            ]),
+    );
+
+    plot.show();
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
- 
+
     let filename = if args.len() == 1 {
         "src/trace_data_bootcamp_PT5000.csv"
     } else {
         &args[1]
     };
 
-    let (plaintexts, _ciphertexts, traces) = read_trace_file(filename);
+    let correlations;
 
-    let recovered_key_byte = compute_cpa(&plaintexts, &traces);
-    println!("Recovered key byte: {:02X}", recovered_key_byte);
+    // Window for the trace data
+    let window = (1300, 1350);
+
+    
+    let mut input = String::new();
+    if std::path::Path::new("correlation_matrix.csv").exists() {
+        println!("Precomputed correlation matrix found. Do you want to use it? (y/n)");
+
+        std::io::stdin().read_line(&mut input).unwrap();
+    }
+
+    if input.trim() == "y" {
+        println!("Using precomputed correlation matrix");
+        correlations = ReaderBuilder::new()
+            .has_headers(false)
+            .from_path("correlation_matrix.csv")
+            .unwrap()
+            .into_records()
+            .map(|record| record.unwrap().iter().map(|x| x.parse().unwrap()).collect())
+            .collect();
+    } else {
+        let (plaintexts, _, traces) = read_trace_file(filename, window);
+
+        correlations = compute_correlation_matrix(&traces, &plaintexts);
+
+        let mut wtr = csv::Writer::from_path("correlation_matrix.csv").unwrap();
+        for row in correlations.iter() {
+            wtr.write_record(row.iter().map(|x| x.to_string())).unwrap();
+        }
+        wtr.flush().unwrap();
+    }
+
+    let key = best_key_guess(&correlations);
+    println!("\nBest key guess: 0x{:02X}", key);
+
+    plot_correlation(&correlations, key, window);
 }
